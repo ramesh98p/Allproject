aea121994b2f63f5f12fa0a39e3d17db16092813
HEMANT <HEMANT@DESKTOP-U6IVH12>
2023-03-01 12:08:49 +0530

    DemoFile

---
+++ src/TestDemo/Demo1.java
@@ -0,0 +1,16 @@
+package TestDemo;
+
+import java.util.Scanner;
+
+public class Demo1 {
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc1=new Scanner(System.in);
+System.out.println("Enter the name");
+String name=sc1.next();
+	}
+
+}
